0a166ed6c34977793ec4fbeffc866e90ade30ad7
Edwin RA <edwinra240@gmail.com>
2022-08-31 21:37:54 -0500

    v4.1: redirec at movies popularity and movies lastest

--- api.js
+++ api.js
@@ -1,7 +1,9 @@
-async function getLatestMovies(moviesUrl) {
-    // let moviesUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${p}`;
+const API_KEY = "d367f0aa1b646902549724b33acb81a6";
+
+function getPopularMovies(p) {
+  async function getLatestMovies() {
+    let moviesUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${p}`;
     try {
-      console.log("Realiza Promise");
       let response = await axios.get(moviesUrl);
       return response.data.results;
     } catch (e) {
@@ -9,3 +11,94 @@ async function getLatestMovies(moviesUrl) {
       return [];
     }
   }
+
+  let latestMovies;
+
+  getLatestMovies().then((peliculas) => {
+    latestMovies = peliculas;
+
+    console.log(latestMovies);
+
+    const buildProductList = (movieList) => {
+      //select the parent HTML tag
+      var getParentElement = document.getElementById("moviesList");
+      getParentElement.innerHTML = "";
+
+      //Loop the product list array in order to generate the <section> </section>
+      movieList.forEach((movie) => {
+        //creating the section cards
+        const newDiv = document.createElement("div");
+
+        //adding a class and style
+        newDiv.classList.add("card");
+        newDiv.classList.add("m-1");
+        newDiv.style.width = "10rem";
+
+        //creating childs into the parent element section
+        newDiv.innerHTML = `
+            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="img">
+            <div class="card-body">
+            <h7 class="card-title fw-bold">${movie.title}</h7>
+            <p class="card-text">Popularidad: ${movie.popularity}</p>
+            <a href="#" class="btn btn-primary">Mas detalles</a>
+            </div>
+        `;
+        getParentElement.appendChild(newDiv);
+      });
+    };
+
+    // window.addEventListener("DOMContentLoaded", () => { });
+    buildProductList(latestMovies);
+  });
+}
+
+function getLatest() {
+  async function getLatestMovies() {
+    let moviesUrl = `https://api.themoviedb.org/3/movie/latest?api_key=${API_KEY}&language=en-US`;
+    try {
+      let response = await axios.get(moviesUrl);
+      return response.data.results;
+    } catch (e) {
+      console.log(e);
+      return [];
+    }
+  }
+
+  let latestMovies;
+  getLatestMovies().then((peliculas) => {
+    latestMovies = peliculas;
+
+    console.log(latestMovies);
+
+    const buildProductList = (movieList) => {
+      //select the parent HTML tag
+      var getParentElement = document.getElementById("moviesList");
+      getParentElement.innerHTML = "";
+
+      //Loop the product list array in order to generate the <section> </section>
+      movieList.forEach((movie) => {
+        //creating the section cards
+        const newDiv = document.createElement("div");
+
+        //adding a class and style
+        newDiv.classList.add("card");
+        newDiv.classList.add("m-1");
+        newDiv.style.width = "10rem";
+
+        //creating childs into the parent element section
+        newDiv.innerHTML = `
+            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="img">
+            <div class="card-body">
+            <h7 class="card-title fw-bold">${movie.title}</h7>
+            <p class="card-text">NOOO: ${movie.popularity}</p>
+            <a href="#" class="btn btn-primary">Mas detalles</a>
+            </div>
+        `;
+        getParentElement.appendChild(newDiv);
+      });
+    };
+
+    // window.addEventListener("DOMContentLoaded", () => { });
+    buildProductList(latestMovies);
+  });
+}
--- main.js
+++ main.js
@@ -1,74 +1,87 @@
 //
-const API_KEY = "d367f0aa1b646902549724b33acb81a6";
-const btnprePage = document.getElementById("prePage");
-const btnnextPage = document.getElementById("nextPage");
+const btnPrePage = document.getElementById("prePage");
+const btnNextPage = document.getElementById("nextPage");
+const btnNews = document.getElementById("btnNews");
+const btnPopulares = document.getElementById("btnPopulares");
 let p = 1;
 
-btnprePage.onclick = () => {
+getPopularMovies(p);
+
+btnPrePage.onclick = () => {
   if (p == 1) {
     p = 1;
   } else {
     p -= 1;
   }
-  latestMovies(p);
+  getPopularMovies(p);
 };
 
-btnnextPage.onclick = () => {
+btnNextPage.onclick = () => {
   p++;
-  latestMovies(p);
+  getPopularMovies(p);
 };
 
-function latestMovies(p) {
-
-  async function getLatestMovies() {
-    let moviesUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${p}`;
-    try {
-      let response = await axios.get(moviesUrl);
-      return response.data.results;
-    } catch (e) {
-      console.log(e);
-      return [];
-    }
-  }
-
-  let latestMovies;
-
-  getLatestMovies().then((peliculas) => {
-    latestMovies = peliculas;
-    
-    console.log(latestMovies);
-
-    const buildProductList = (movieList) => {
-      //select the parent HTML tag
-      var getParentElement = document.getElementById("moviesList");
-      getParentElement.innerHTML = "";
-
-      //Loop the product list array in order to generate the <section> </section>
-      movieList.forEach((movie) => {
-        //creating the section cards
-        const newDiv = document.createElement("div");
-
-        //adding a class and style
-        newDiv.classList.add("card");
-        newDiv.classList.add("m-1");
-        newDiv.style.width = "10rem";
-
-        //creating childs into the parent element section
-        newDiv.innerHTML = `
-          <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="img">
-          <div class="card-body">
-          <h7 class="card-title fw-bold">${movie.title}</h7>
-          <p class="card-text">${movie.popularity}</p>
-          <a href="#" class="btn btn-primary">Mas detalles</a>
-          </div>
-      `;
-        getParentElement.appendChild(newDiv);
-      });
-    };
-
-    // window.addEventListener("DOMContentLoaded", () => { });
-    buildProductList(latestMovies);
-  });
+btnPopulares.onclick = () => {
+  p = 1;
+  getPopularMovies(p);
 }
 
-latestMovies(p);
+btnNews.onclick = () => {
+  getLatest();
+}
+
+
+
+// function latestMovies(p) {
+
+//   async function getLatestMovies() {
+//     let moviesUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${p}`;
+//     try {
+//       let response = await axios.get(moviesUrl);
+//       return response.data.results;
+//     } catch (e) {
+//       console.log(e);
+//       return [];
+//     }
+//   }
+
+//   let latestMovies;
+
+//   getLatestMovies().then((peliculas) => {
+//     latestMovies = peliculas;
+    
+//     console.log(latestMovies);
+
+//     const buildProductList = (movieList) => {
+//       //select the parent HTML tag
+//       var getParentElement = document.getElementById("moviesList");
+//       getParentElement.innerHTML = "";
+
+//       //Loop the product list array in order to generate the <section> </section>
+//       movieList.forEach((movie) => {
+//         //creating the section cards
+//         const newDiv = document.createElement("div");
+
+//         //adding a class and style
+//         newDiv.classList.add("card");
+//         newDiv.classList.add("m-1");
+//         newDiv.style.width = "10rem";
+
+//         //creating childs into the parent element section
+//         newDiv.innerHTML = `
+//           <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="img">
+//           <div class="card-body">
+//           <h7 class="card-title fw-bold">${movie.title}</h7>
+//           <p class="card-text">${movie.popularity}</p>
+//           <a href="#" class="btn btn-primary">Mas detalles</a>
+//           </div>
+//       `;
+//         getParentElement.appendChild(newDiv);
+//       });
+//     };
+
+//     // window.addEventListener("DOMContentLoaded", () => { });
+//     buildProductList(latestMovies);
+//   });
+// }
+
