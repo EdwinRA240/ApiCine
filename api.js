908e3b2f3e1ab577dd605433b5924f2ae439d2b3
Edwin RA <edwinra240@gmail.com>
2022-09-01 21:13:39 -0500

    v4.1: add move new and popular

--- api.js
+++ api.js
@@ -40,7 +40,7 @@ function getPopularMovies(p) {
             <div class="card-body">
             <h7 class="card-title fw-bold">${movie.title}</h7>
             <p class="card-text">Popularidad: ${movie.popularity}</p>
-            <a href="#" class="btn btn-primary">Mas detalles</a>
+            <a href="#" class="btn btn-primary" id="btnMasDetalles">Mas detalles</a>
             </div>
         `;
         getParentElement.appendChild(newDiv);
@@ -52,9 +52,9 @@ function getPopularMovies(p) {
   });
 }
 
-function getLatest() {
+function getTopMovies(p) {
   async function getLatestMovies() {
-    let moviesUrl = `https://api.themoviedb.org/3/movie/latest?api_key=${API_KEY}&language=en-US`;
+    let moviesUrl = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=${p}`;
     try {
       let response = await axios.get(moviesUrl);
       return response.data.results;
@@ -65,6 +65,7 @@ function getLatest() {
   }
 
   let latestMovies;
+
   getLatestMovies().then((peliculas) => {
     latestMovies = peliculas;
 
@@ -90,11 +91,68 @@ function getLatest() {
             <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="img">
             <div class="card-body">
             <h7 class="card-title fw-bold">${movie.title}</h7>
-            <p class="card-text">NOOO: ${movie.popularity}</p>
-            <a href="#" class="btn btn-primary">Mas detalles</a>
+            <p class="card-text">Calificación: ${movie.vote_average}</p>
+            <a href="#" class="btn btn-primary" id="btnMasDetalles">Mas detalles</a>
             </div>
         `;
         getParentElement.appendChild(newDiv);
+      });
+    };
+
+    // window.addEventListener("DOMContentLoaded", () => { });
+    buildProductList(latestMovies);
+  });
+}
+
+function getId(id) {
+  async function getLatestMovies() {
+    let moviesUrl = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`;
+    try {
+      let response = await axios.get(moviesUrl);
+      return response.data.results;
+    } catch (e) {
+      console.log(e);
+      return [];
+    }
+  }
+
+  let latestMovies;
+  getLatestMovies().then((peliculas) => {
+    latestMovies = peliculas;
+
+    console.log(latestMovies);
+
+    const buildProductList = (movieList) => {
+      //select the parent HTML tag
+      var getParentElement = document.getElementById("moviesList");
+      getParentElement.innerHTML = "";
+
+      //Loop the product list array in order to generate the <section> </section>
+      movieList.forEach((movie) => {
+        var id = movie.id;
+        //creating the section cards
+        const newDiv = document.createElement("div");
+
+        // adding a class and style
+        newDiv.classList.add("carousel");
+        newDiv.classList.add("slide");
+
+        //creating childs into the parent element sectio11n
+        newDiv.innerHTML = `1
+            <div class="carousel-indicators">
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>${movie.title}</h5>
+                    <p>Descripion: ${movie.overview}</p>
+                </div>
+                </div>
+            </div>
+          `;
+        getParentElement.appendChild(newDiv);
+        return id;
       });
     };
 
--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-//
 const btnPrePage = document.getElementById("prePage");
 const btnNextPage = document.getElementById("nextPage");
 const btnNews = document.getElementById("btnNews");
 const btnPopulares = document.getElementById("btnPopulares");
+const btnMasDetalles = document.getElementById("btnMasDetalles");
 let p = 1;
 
-getPopularMovies(p);
+window.addEventListener("DOMContentLoaded", () => { 
+  getPopularMovies(p);
+}); 
 
 btnPrePage.onclick = () => {
   if (p == 1) {
@@ -13,75 +15,53 @@ btnPrePage.onclick = () => {
   } else {
     p -= 1;
   }
-  getPopularMovies(p);
 };
 
 btnNextPage.onclick = () => {
   p++;
-  getPopularMovies(p);
 };
 
 btnPopulares.onclick = () => {
   p = 1;
   getPopularMovies(p);
+  
+  btnPrePage.onclick = () => {
+    if (p == 1) {
+      p = 1;
+    } else {
+      p -= 1;
+    }
+    getPopularMovies(p);
+  };
+  
+  btnNextPage.onclick = () => {
+    p++;
+    getPopularMovies(p);
+  };
 }
 
 btnNews.onclick = () => {
-  getLatest();
+  p= 1;
+  getTopMovies(p);
+  
+  btnPrePage.onclick = () => {
+    if (p == 1) {
+      p = 1;
+    } else {
+      p -= 1;
+    }
+    getTopMovies(p);
+    // getPopularMovies(p);
+  };
+  
+  btnNextPage.onclick = () => {
+    p++;
+    getTopMovies(p);
+    // getPopularMovies(p);
+  };
 }
 
-
-
-// function latestMovies(p) {
-
-//   async function getLatestMovies() {
-//     let moviesUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${p}`;
-//     try {
-//       let response = await axios.get(moviesUrl);
-//       return response.data.results;
-//     } catch (e) {
-//       console.log(e);
-//       return [];
-//     }
-//   }
-
-//   let latestMovies;
-
-//   getLatestMovies().then((peliculas) => {
-//     latestMovies = peliculas;
-    
-//     console.log(latestMovies);
-
-//     const buildProductList = (movieList) => {
-//       //select the parent HTML tag
-//       var getParentElement = document.getElementById("moviesList");
-//       getParentElement.innerHTML = "";
-
-//       //Loop the product list array in order to generate the <section> </section>
-//       movieList.forEach((movie) => {
-//         //creating the section cards
-//         const newDiv = document.createElement("div");
-
-//         //adding a class and style
-//         newDiv.classList.add("card");
-//         newDiv.classList.add("m-1");
-//         newDiv.style.width = "10rem";
-
-//         //creating childs into the parent element section
-//         newDiv.innerHTML = `
-//           <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="img">
-//           <div class="card-body">
-//           <h7 class="card-title fw-bold">${movie.title}</h7>
-//           <p class="card-text">${movie.popularity}</p>
-//           <a href="#" class="btn btn-primary">Mas detalles</a>
-//           </div>
-//       `;
-//         getParentElement.appendChild(newDiv);
-//       });
-//     };
-
-//     // window.addEventListener("DOMContentLoaded", () => { });
-//     buildProductList(latestMovies);
-//   });
-// }
-
+btnMasDetalles.onclick = () => {
+  getId(p);
+  console.log(p);
+};
